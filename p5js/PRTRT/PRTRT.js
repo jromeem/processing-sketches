25432d4297e223b4114a472eef9271197fa5c84f
Jerome Martinez <jerome.e.martinez@gmail.com>
2016-09-19 02:20:28 -0500

    phyllotaxis

--- p5js/PRTRT/PRTRT.js
+++ p5js/PRTRT/PRTRT.js
@@ -1,10 +1,30 @@
-console.log('hello PRTRT!');
+console.log('hello P!');
+
+var colorWheel = 0.0;
+
 function setup() {
-    var canvas = createCanvas(500, 500);
+    var canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("p5canvas");
 }
 
 function draw() {
-    background(100);
-    ellipse(200, 200, 100, 100);
+    background(0,0,0,0.3);
+    
+    noStroke();
+    colorMode(HSB);
+    fill(colorWheel, 100, 100, 0.05);
+    ellipse(mouseX, mouseY, 300, 300);
+    
+    fill(colorWheel, 100, 100, 0.1);
+    ellipse(mouseX, mouseY, 250, 250);
+
+
+    colorMode(HSB);
+    strokeWeight(200);
+    stroke(colorWheel, 200, 200);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    line(ptouchX, ptouchY, touchX, touchY);
+
+    colorWheel+= 0.5;
+    colorWheel = colorWheel % 355;
 }
