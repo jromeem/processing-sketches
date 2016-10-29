bb5dc2a49af70e8dc0bae656a9324e7f37bdfc2b
Jerome Martinez <jerome.e.martinez@gmail.com>
2016-10-29 15:04:48 -0500

    yay blocking is working now

--- p5js/sound_steps/sound_steps.js
+++ p5js/sound_steps/sound_steps.js
@@ -22,21 +22,44 @@ function setup() {
 }
 
 function draw() {
-    background(255);
+    // background(255);
+    noStroke();
     for (var i=0; i<grid.locs.length; i++) {
         var thisLocation = grid.locs[i];
-        var biggestSize = grid.biggestPossible(i);
 
         // if the current location isn't occupied
-        // if (!thisLocation.occupied) {
-        //     var biggestSize = grid.biggestPossible(i);
-        //     console.log("the biggest so far", biggestSize);
-        //     var step = new SoundStep(thisLocation.x, thisLocation.y, biggestSize);
-        //     steps.push(step);
-        // } else {
-        //     continue;
-        // }
+        if (!thisLocation.occupied) {
+            var randomFit = int(random(1, grid.biggestPossible(i)+1));
+            var step = new SoundStep(thisLocation.x, thisLocation.y, randomFit);
+            grid.blockOff(i, randomFit);
+            steps.push(step);
+        } else {
+            continue;
+        }
+
+        if (i > 0) {
+            break;
+        }
     }
+
+    // debugging
+    for (var i=0; i<grid.locs.length; i++) {
+        var thisLoc = grid.locs[i];
+        stroke(255,100,100);
+        if (thisLoc.occupied) {
+            fill(255,100,100,200);
+        } else {
+            noFill();
+        }
+        rect(grid.locs[i].x,grid.locs[i].y, baseSize, baseSize);
+    }
+    // end debug
+
+    for (var i=0; i<steps.length; i++) {
+        steps[i].display();
+    }
+    console.log("steps!!", steps);
+    console.log("steps!!", grid);
 }
 
 ////////////////////////////////
@@ -49,7 +72,7 @@ function SoundStep(x, y, s) {
 }
 SoundStep.prototype.display = function() {
     fill(random(255),70,100);
-    rect(this.posx+padding, this.posy+padding, this.stepSize-padding*2, this.stepSize-padding*2);
+    rect(this.posx+padding, this.posy+padding, this.stepSize*baseSize-padding*2, this.stepSize*baseSize-padding*2);
 };
 
 
@@ -98,6 +121,16 @@ Grid.prototype.biggestPossible = function(index) {
     // return the smallest scale out of the two with each constrained to 3 as the max scale
     var smallestScale = min(constrain(nextRight, 1, 3), constrain(nextDown, 1, 3));
     return smallestScale;
+};
+// given a size and a location, block off locations as occupied
+Grid.prototype.blockOff = function(index, givenScale) {
+    var yOffset = canvasSize / baseSize;
+    for (var y=0; y<givenScale*yOffset; y+=yOffset) {
+        for (var x=0; x<givenScale; x++) {
+            console.log(index, x, y);
+            this.locs[x+y+index].occupied = true;
+        }
+    }
 };
 
 ///////////////////////////////
